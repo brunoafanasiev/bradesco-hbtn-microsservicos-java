28949df13e7d24bb9ccb406636c0be4cc9cd3fad
Bruno Afanasiev <bruno.afanasiev@gmail.com>
2025-10-20 17:35:15 -0300

    teste

---
+++ 0x04/config_JPA+H2_database/com/example/jpa_h2_demo/JpaH2DemoApplication.java
@@ -0,0 +1,10 @@
+package com.example.jpa_h2_demo;
+
+@SpringBootApplication
+public class JpaH2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaH2DemoApplication.class, args);
+	}
+
+}
